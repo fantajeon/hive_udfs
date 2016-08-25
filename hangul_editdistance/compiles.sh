90e4c51ccf852b7277acc3b9d718b7ef6acb7c23
Hyeok <fantajeon@naver.com>
2016-08-25 15:00:54 +0900

    add hadoop library

--- hangul_editdistance/compiles.sh
+++ hangul_editdistance/compiles.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# libraries download url: https://archive.cloudera.com/cdh5/cdh/5/
 hive_jar=$(ls /usr/local/Cellar/hive/2.1.0/libexec/lib/hive-exec-*.jar)
 #hadoop_jar='/usr/local/Cellar/hadoop/2.7.2/libexec/share/hadoop/common/hadoop-common-2.7.2.jar'
 hadoop_jar='/usr/local/Cellar/hadoop/hadoop-2.6.0-cdh5.5.1/share/hadoop/common/hadoop-common-2.6.0-cdh5.5.1.jar'
